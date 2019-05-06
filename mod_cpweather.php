ea804c0e37d1904b1a3f968c5c4b7d420e9ad0a5
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-05-06 09:06:56 -0400

    Updated server side to work with dark sky.

--- mod_cpweather.php
+++ mod_cpweather.php
@@ -10,17 +10,14 @@ if(isset($_POST['mod_cpweather'])) {
 		if(isset($data['time']) && !empty($data['time']) && $data['time'] > time() - 346) {
 			echo $data['weather'];
 		} else {
-			$apiUrl = 'http://api.wunderground.com/api/'.$settings['wu_api_key'].'/conditions/q/'.$settings['wu_location'].'.json';
+			$apiUrl = 'https://api.darksky.net/forecast/'.$settings['wu_api_key'].'/'.$settings['wu_location'];
 			ini_set('default_socket_timeout', 3);
 			$response = @file_get_contents($apiUrl);
 			if(!empty($response)) {
 				$json = json_decode($response);
-				if(isset($json->current_observation) && !empty($json->current_observation)) {
-					$json->icon = str_replace('http://icons.wxug.com/i/c/k/','',str_replace('.gif','',$json->icon_url));
-					$weather = json_encode($json->current_observation);
-					putDB(array('time'=>time(), 'weather'=>$weather), 'cache');
-					echo $weather;
-				}
+				$weather = json_encode($json);
+				putDB(array('time'=>time(), 'weather'=>$weather), 'cache');
+				echo $weather;
 			} else {
 				if(isset($data['weather']) && !empty($data['weather'])) {
 					echo $data['weather'];
